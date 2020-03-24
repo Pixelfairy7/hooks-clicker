7bd7735b565ef1604e56d3c519b328eb0b027b89
Erika Myles <erikamyles314@gmail.com>
2020-03-23 22:27:18 -0400

    Updates

---
+++ precache-manifest.02a8e1ee154c6e3593c73201260ea8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86424955b825df040474b1371ee3eafe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc9d4d48df8047170290",
+    "url": "/static/css/main.91314d1e.chunk.css"
+  },
+  {
+    "revision": "73ad0573335aa5b25def",
+    "url": "/static/js/2.44e002ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.44e002ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc9d4d48df8047170290",
+    "url": "/static/js/main.51e7735f.chunk.js"
+  },
+  {
+    "revision": "0ce478dc50097bd5fbab",
+    "url": "/static/js/runtime-main.ddbbcc0a.js"
+  }
+]);
